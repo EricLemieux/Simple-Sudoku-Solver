dd61dfa2ae31a6a0c9fa40c4c49338541faa94d1
Eric Lemieux <ericlemieux2@gmail.com>
2014-05-13 12:34:02 -0400

    Basic plan and sample puzzle to solve

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,12 @@
 //Simple Sudoku Solver
 //Eric Lemieux, 2014
 
+//Basic plan:
+//1) Load the puzzle from a text file
+//2) Solve the puzzle 
+//3) Print the results
+//4) Ask the user if they want to solve another puzzle of exit.
+
 int main()
 {
 	return 0;
