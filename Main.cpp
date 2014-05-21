f7828f9dfc4f9cda50f54c7cc2b062e63585c567
Eric Lemieux <ericlemieux2@gmail.com>
2014-05-21 13:44:52 -0400

    Puzzle file loading

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,69 @@
 //3) Print the results
 //4) Ask the user if they want to solve another puzzle of exit.
 
+#include <iostream>
+#include <fstream>
+
+//Predefined functions
+char*	Load(char *filePath);
+char*	Solve(char *puzzle);
+void	Print(char *puzzle);
+
 int main()
 {
+	char *arr = new char[81];
+	arr = Load("puzzle.txt");
+
 	return 0;
+}
+
+//Load the puzzle file for the program to solve.
+//Takes in the path to the file
+//Outputs the puzzle
+//Currently assumes the file is correct, may add some checking later
+char* Load(char *filePath)
+{
+	std::ifstream file;
+	file.open(filePath);
+
+	char current;
+	char *puzzle = new char[81];
+	int count = 0;
+
+	while (file.is_open())
+	{
+		file.get(current);
+
+		//Check to see if the line is a comment
+		if (current == '#')
+		{
+			//Skip this line because it is only comments for the user.
+			file.ignore(256, '\n');
+		}
+		else
+		{
+			if (current != '\n')
+			{
+				if (count == 81)
+				{
+					return puzzle;
+				}
+				puzzle[count] = current;
+				count += 1;
+			}
+		}
+	}
+
+	return puzzle;
+}
+
+char* Solve(char *puzzle)
+{
+	//TODO
+	return NULL;
+}
+
+void Print(char *puzzle)
+{
+	//TODO
 }
